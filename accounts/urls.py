436e1676e717323403ffd47bfcd80972022e463f
sachinaryal <saryal@assurance.com>
2021-08-27 16:26:52 +0545

    Authentication work done.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import user_passes_test
+
+
 urlpatterns = [
-    path('',views.index,name="home"),
-    path('sign_up/',views.sign_up,name="sign-up"),
-    path('login/',views.login_user,name="login"),
-    path('logout/',views.logout_user,name="logout"),
+    path('', views.login_user, name="login"),
+    path('sign_up/', views.sign_up, name="sign-up"),
+    path('login/', views.login_user, name="login"),
+    path('logout/', views.logout_user, name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-# Create your views here.
-@login_required
-def index(request):
-    return render(request,'accounts/index.html')
+
 
 def sign_up(request):
     context = {}
@@ -16,24 +12,30 @@ def sign_up(request):
     if request.method == "POST":
         if form.is_valid():
             user = form.save()
-            login(request,user)
-            return render(request,'accounts/index.html')
-    context['form']=form
-    return render(request,'registration/sign_up.html',context)
+            login(request, user)
+            return redirect('/')
+    context['form'] = form
+    return render(request, 'sign_up.html', context)
+
 
 def login_user(request):
+    context = {}
+    form = AuthenticationForm(request.POST or None)
+    context['form'] = form
     if request.method == 'POST':
-        user = authenticate(username=request.POST['username'],password = request.POST['password'])
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
         if user is not None:
-            login(request,user)
-            return render(request,'accounts/index.html')
+            login(request, user)
+            return redirect('/')
         else:
-            return render (request,'accounts/login.html', {'error':'Username or password is incorrect!'})
+            context['error'] = 'Username or password is incorrect!'
+            return render(request, 'login.html', context)
     else:
-        return render(request,'accounts/login.html')
+        return render(request, 'login.html', context)
+
 
 def logout_user(request):
-    if request.method == 'POST':
-        logout(request)
-    return render(request,'accounts/logged_out.html')
-
+    # if request.method == 'POST':
+    #     logout(request)
+    logout(request)
+    return redirect('/accounts/login')
