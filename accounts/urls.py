be6b61db87da7de83606fe4a2237e95bcd67442c
Preranakhadka <58589976+Preranakhadka@users.noreply.github.com>
2021-08-27 13:58:32 +0545

    changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name="home"),
-    path('accounts/sign_up/',views.sign_up,name="sign-up")
+    path('sign_up/',views.sign_up,name="sign-up"),
+    path('login/',views.login_user,name="login"),
+    path('logout/',views.logout_user,name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def index(request):
     return render(request,'accounts/index.html')
+
 def sign_up(request):
     context = {}
     form = UserCreationForm(request.POST or None)
@@ -19,3 +20,20 @@ def sign_up(request):
             return render(request,'accounts/index.html')
     context['form']=form
     return render(request,'registration/sign_up.html',context)
+
+def login_user(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'],password = request.POST['password'])
+        if user is not None:
+            login(request,user)
+            return render(request,'accounts/index.html')
+        else:
+            return render (request,'accounts/login.html', {'error':'Username or password is incorrect!'})
+    else:
+        return render(request,'accounts/login.html')
+
+def logout_user(request):
+    if request.method == 'POST':
+        logout(request)
+    return render(request,'accounts/logged_out.html')
+
