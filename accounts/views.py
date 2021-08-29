ec89d149f0112c05d4b1099caec2dd3cc1160d01
Preranakhadka <58589976+Preranakhadka@users.noreply.github.com>
2021-08-29 20:57:00 +0545

    new changes

--- accounts/views.py
+++ accounts/views.py
@@ -19,6 +19,7 @@ def sign_up(request):
 
 
 def login_user(request):
+	
     context = {}
     form = AuthenticationForm(request.POST or None)
     context['form'] = form
@@ -26,7 +27,7 @@ def login_user(request):
         user = authenticate(username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             login(request, user)
-            return redirect('index.html')
+            return redirect('/')
         else:
             context['error'] = 'Username or password is incorrect!'
             return render(request, 'login.html', context)
