9c88459200416863d81670e8e5054d26609056c3
Preranakhadka <58589976+Preranakhadka@users.noreply.github.com>
2021-08-30 21:54:02 +0545

    reformatted.

--- photogallery/models.py
+++ photogallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+def user_directory_path(instance, filename):
+    return '{0}/{1}'.format(instance.id, filename)
 
 # Create your models here.
+class Image(models.Model):
+	title=models.CharField(max_length=255)
+	photo=models.ImageField(upload_to = user_directory_path)
+	user=models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+	description=models.CharField(max_length=255)
+	date=models.DateTimeField(auto_now_add=True)
