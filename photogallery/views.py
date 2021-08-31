d61ee9ef0d2f0421a12c44767ff350a8a93d9793
Preranakhadka <58589976+Preranakhadka@users.noreply.github.com>
2021-08-31 09:14:10 +0545

    changes

--- photogallery/views.py
+++ photogallery/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Image
 
 
 @login_required
 def home(request):
-    return render(request, 'index.html')
+     if request.method=="POST":
+     	img=Item()
+     	img.title=request.POST.get('title')
+     	img.description=request.POST.get('description')
+
+     	if len(request.FILES)!=0:
+     		img.photo=request.FILES['photo']
+     	img.save()
+     	return redirect(request, 'index.html')
