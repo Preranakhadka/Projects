f9ee90e9919c9f34ceeae7439eaabdea80f1bd2c
Preranakhadka <58589976+Preranakhadka@users.noreply.github.com>
2021-09-03 06:52:09 +0545

    add changes

--- accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Image(models.Model):
-	id=models.CharField(max_length=3,primary_key=True)
-	title=models.CharField(max_length=255)
-	photo=models.ImageField(upload_to="images")
-	description=models.CharField(max_length=255)
-	date=models.DateTimeField(auto_now_add=True)
