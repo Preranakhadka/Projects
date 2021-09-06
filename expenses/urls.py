f5bfce6b05d53922f5c763aab56bdc1dc3b2127c
Preranakhadka <58589976+Preranakhadka@users.noreply.github.com>
2021-09-06 16:29:23 +0545

    message

--- expenses/urls.py
+++ expenses/urls.py
@@ -7,4 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('expenses/', views.new_expenses, name="expenses"),
+    path('expenses/', views.edit_expenses, name="expenses"),
+
+
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Expense
 
+
 # Create your views here.
 
 @login_required
@@ -19,4 +20,44 @@ def new_expenses(request):
 		items = request.POST['items']
 		Expense(price=price, category=category, user=user, items=items).save()
 	return redirect('/expenses/')
+
+
+def edit_expenses(request):
+	post=get_object_or_404(POST,Expense)
+	if request.method=="POST":
+		price = price(request.POST,instance=post)
+		category = category(request.POST,instance=post)
+		user = request.user
+		items = items(request.POST,instance=post)
+		Expense(price=price, category=category, user=user, items=items).save()
+	return redirect('/expenses/')
+	else:
+		expenditure=expenditure(instance=post)
+		expenses='expenses/index_expenses.html'
+		context={expenditure:expenditure}
+		return render (request,expenses,context)
+
+		
+
+def delete_expenses(request):
+	post=get_object_or_404(POST,Expense)
+	if request.method=="POST":
+		price = price(request.POST,instance=post)
+		category = category(request.POST,instance=post)
+		user = request.user
+		items = items(request.POST,instance=post)
+		Expense(price=price, category=category, user=user, items=items).save()
+	return redirect('/expenses/')
+	else:
+		expenditure=expenditure(instance=post)
+		expenses='expenses/index_expenses.html'
+		context={expenditure:expenditure}
+		return render (request,expenses,context)
+   
+#    
+
+
+
+
+
 
